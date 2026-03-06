2db481282454623678b1dc24f43405a34a938821
xzmmmmmmmm <3516382071@qq.com>
2026-03-06 20:25:07 +0800

    检测config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+
+
 import torch
 print(f"WSL 是否识别 GPU: {torch.cuda.is_available()}")
 print(f"显卡型号: {torch.cuda.get_device_name(0)}")
-print(f"算力版本: {torch.cuda.get_device_capability(0)}") 
-# RTX 5060 应该显示 (12, 0) 左右
+print(f"计算能力: {torch.cuda.get_device_capability(0)}") 
+# 期待输出: (12, 0)
+exit()
